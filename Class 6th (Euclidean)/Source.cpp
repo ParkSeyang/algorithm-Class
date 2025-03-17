1ea136d813f6b275e0bae43561db273b4fc9c649
ParkSeyang <ghlwkd14@naver.com>
2025-03-17 19:43:43 +0900

    (2025.03.17) 거품정렬

---
+++ Class 6th (Euclidean)/Source.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+int& euclid(int x, int y)
+{
+	if (y == 0)
+	{
+		return x;
+	}
+	else
+	{
+		return euclid(y, x % y);
+	}
+
+	// 1. 두개의 자연수 x,y 가 있습니다
+	// 그리고 x와 y를 나눈 나머지 를 R 이라고 합니다.
+	// 2. R이 0이라면, x 와 y의 최대공약수 y가 됩니다.
+
+	// 3. R이 0이 아니라면, x와 y의 최대공약수는 y와 R의
+	//	최대공약수와 같기 때문에 x에는 y그리고 y에는
+	//	R을 대입한후 1번으로 돌아갑니다.
+}
+int main()
+{
+#pragma region 유클리드 호제버
+	// 2개의 자연수 또는 정식의 최대 공약수를 구하는 방식으로
+	// 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘 입니다.
+
+	int x = 145;
+	int y = 13;
+
+	cout << euclid(x, y) << endl;
+
+	// int a = 156;
+	// int b = 60;
+	// int r = 0;
+	// 
+	// 
+	// for (int i = 1; i <= a && i <= b; i++)
+	// {
+	// 	if (a % i == 0 && b % i == 0)
+	// 	{
+	// 		r = i;
+	// 	}
+	// }
+	// 
+	// cout << "최대공약수 : " << r << endl;
+#pragma endregion
+
+	return 0;
+}
+
+
+
--- Program (Algorithm)/Source.cpp
+++ Program (Algorithm)/Source.cpp
@@ -2,50 +2,8 @@
 
 using namespace std;
 
-#define SIZE 5
-
 int main()
 {
-#pragma region 삽입 정렬
-	// 데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여
-	// 자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘 입니다.
-
-	// key 라는변수 
-	// key는 처음인덱스값이아닌 그다음번째 인덱스값을넣어야한다
-
-	// 그리고 0번째인덱스와 값을비교한다
-	// 2번의 값보다 1번의값이크다면 1번의값을 2번에 삽입한다.
-
-	// 그리고 키값을 인덱스 1번에 삽입한다.
-
-	int list[SIZE] = { 9,6,8,1,3 };
-
-	int key = 0;
-
-	for (int i =0; i < SIZE - 1; i++)
-	{
-		key = list[i + 1]; // key는 다음 값을 가리킨다
-		for (int j = 0; j < i + 1; j++)
-		{
-			if (list[i-j] > list[i-j+1]) // 키값과 키의다음값을 비교하고 키값보다 클경우 다음칸으로 밀어낸다
-			{
-				list[i - j + 1] = list[i - j];
-				list[i - j] = key;
-			}
-			else if (list[i - j] < list[i - j + 1]) // 만약 그전값이 키값보다 크다면 다음 값을 비교하도록 한다.
-			{
-				continue;
-			}
-		}
-
-	}
-
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << list[i] << " ";
-	}
-
-#pragma endregion
 
 	return 0;
 }
