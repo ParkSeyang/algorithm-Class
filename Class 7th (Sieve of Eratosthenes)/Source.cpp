434c8ba1be501ddab26caab2cb1223f0a5b537e6
ParkSeyang <ghlwkd14@naver.com>
2025-03-19 17:56:00 +0900

    dfdf

---
+++ Class 7th (Sieve of Eratosthenes)/Source.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool isPrime(/*const*/ int n)
+{
+	if (n <= 1)
+	{
+		return false;
+	}
+
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void sieve()
+{
+
+}
+
+int main()
+{
+	cout << isPrime(11) << endl;
+	return 0;
+}
+
+
