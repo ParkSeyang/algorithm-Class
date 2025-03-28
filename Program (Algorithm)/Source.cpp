121df385566187968072133b58c8f16de65f5068
ParkSeyang <ghlwkd14@naver.com>
2025-03-28 19:43:32 +0900

    (2025.03.28) 깊이 우선 탐색

--- Program (Algorithm)/Source.cpp
+++ Program (Algorithm)/Source.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
+#include <queue>
 
 #define SIZE 8
 
@@ -8,7 +9,9 @@ using namespace std;
 class Node
 {
 private:
+	// 인접 리스트와 탐색할때는 Queue가 필요하다.
 	bool visited[SIZE];
+	queue<int> queue;
 	vector<int> adjacencyList[SIZE];
 public:
 
@@ -25,59 +28,63 @@ public:
 		adjacencyList[i].push_back(j);
 		adjacencyList[j].push_back(i);
 	}
-	void Search(int start)
+	void search(int start)
 	{
+		queue.push(start);
+
 		visited[start] = true;
-		for (int i = 0; i < SIZE; i++)
+
+		while (queue.empty() == false)
 		{
-			if (visited != NULL)
+			int print = queue.front();
+
+			queue.pop();
+
+			cout << print << " ";
+
+			for (int i = 0; i < adjacencyList[print].size(); i++)
 			{
-				adjacencyList[start]???
+				int next = adjacencyList[print][i];
+				if (visited[next] == false)
+				{
+					queue.push(next);
+
+					visited[next] = true;
+				}
 			}
 		}
-	
-		
+		// 방문한노드를 true로 바꿈
+		// 1에서 시작해서 가장가까운 노드로 접근함2,3 
+		// 임시변수를 만든다
+		// 임시변수에는 방문한 노드의 값을대입하고 출력한다.
 
-	// 값이 대입되면 visited 배열 값에 true 를 넣어준다
-		// 1에서 부터시작된 값은 자신과 가장인접한 노드로 이동한다.
-		// 노드중에서 방문되지않은노드가있다면 방문을 하고 만약 방문을햇다면 종료하면서 하나씩 값을뺍니다.
 	}
 };
 
-
 int main()
 {
-#pragma region 깊이 우선 탐색 (Depth First Search)
+#pragma region 너비 우선 탐색 (Breadth First Search)
+	// 시작 정점을 방문한 후 시작 정점에 인접한
+	// 모든 정점들을 우선 방문하는 방법입니다.
 
-	// root 노드에서 부터 다음 분기로 다음 분기로 넘어가기 전에
-	// 해당 분기를 완벽하게 탐색하는 방법입니다.
 
-	// 깊이 우선 탐색은 자료 구조 (Stack) 을 활용합니다.
 
-	// 1. 시작 노드를 스택에 넣고 방문 처리를 합니다.
-
-	// 2. 스택의 최상단 노드에 방문하지 않은 인접 노드가
-	//    있으면 그 노드를 스택에 넣고 방문 처리합니다.
-
-	// 3. 방문하지 않은 인접 노드가 없으면 스택에서 최상단에
-	//    있는 노드를 꺼냅니다.
-
-	// 4. 더 이상 2번의 과정을 수행할 수 없을 때 까지 반복합니다.
+	// 더 이상 방문하지 않은 정점이 없을 때까지 방문하지 않은 
+	// 모든 정점들에 대해서도 너비 우선 탐색을 적용합니다.
 
 	Node node;
-	
+
 	node.Insert(1, 2);
 	node.Insert(1, 3);
 
-	node.Insert(2, 2);
 	node.Insert(2, 4);
 	node.Insert(2, 5);
 
 	node.Insert(3, 6);
 	node.Insert(3, 7);
 
-	node.Insert(4, 5);
-	node.Insert(6, 7);
+	node.search(1);
+
 #pragma endregion
 
 	return 0;
