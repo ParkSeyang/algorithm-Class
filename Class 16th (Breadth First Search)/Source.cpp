2945d6fd9c11f684d9534b6bcceec5c4b57dc40c
ParkSeyang <ghlwkd14@naver.com>
2025-03-31 19:49:44 +0900

    (2025.03.31) 너비 우선 탐색

---
+++ Class 16th (Breadth First Search)/Source.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+#define SIZE 8
+
+using namespace std;
+
+class Node
+{
+private:
+	// 인접 리스트와 탐색할때는 Queue가 필요하다.
+	bool visited[SIZE];
+	queue<int> queue;
+	vector<int> adjacencyList[SIZE];
+public:
+
+	Node()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			visited[i] = false;
+		}
+	}
+
+	void Insert(int i, int j)
+	{
+		adjacencyList[i].push_back(j);
+		adjacencyList[j].push_back(i);
+	}
+	void search(int start)
+	{
+		queue.push(start);
+
+		visited[start] = true;
+
+		while (queue.empty() == false)
+		{
+			int print = queue.front();
+
+			queue.pop();
+
+			cout << print << " ";
+
+			for (int i = 0; i < adjacencyList[print].size(); i++)
+			{
+				int next = adjacencyList[print][i];
+				if (visited[next] == false)
+				{
+					queue.push(next);
+
+					visited[next] = true;
+				}
+			}
+		}
+		// 방문한노드를 true로 바꿈
+		// 1에서 시작해서 가장가까운 노드로 접근함2,3 
+		// 임시변수를 만든다
+		// 임시변수에는 방문한 노드의 값을대입하고 출력한다.
+
+	}
+};
+
+int main()
+{
+#pragma region 너비 우선 탐색 (Breadth First Search)
+	// 시작 정점을 방문한 후 시작 정점에 인접한
+	// 모든 정점들을 우선 방문하는 방법입니다.
+
+
+
+	// 더 이상 방문하지 않은 정점이 없을 때까지 방문하지 않은 
+	// 모든 정점들에 대해서도 너비 우선 탐색을 적용합니다.
+
+	Node node;
+
+	node.Insert(1, 2);
+	node.Insert(1, 3);
+
+	node.Insert(2, 4);
+	node.Insert(2, 5);
+
+	node.Insert(3, 6);
+	node.Insert(3, 7);
+
+	node.search(1);
+
+#pragma endregion
+
+	return 0;
+}
+
+
+
--- Program (Algorithm)/Source.cpp
+++ Program (Algorithm)/Source.cpp
@@ -1,91 +1,94 @@
 ﻿#include <iostream>
-#include <vector>
 #include <queue>
-
-#define SIZE 8
+#include <vector>
 
 using namespace std;
 
-class Node
+#define SIZE 8
+
+class Graph
 {
 private:
-	// 인접 리스트와 탐색할때는 Queue가 필요하다.
-	bool visited[SIZE];
-	queue<int> queue;
-	vector<int> adjacencyList[SIZE];
+	int degree[SIZE]; // 배열
+	queue<int> queue; // Queue
+	vector<int> adjacencyList[SIZE]; // 인접 리스트
+	
 public:
-
-	Node()
+	Graph()
 	{
 		for (int i = 0; i < SIZE; i++)
 		{
-			visited[i] = false;
+			degree[i] = 0;
 		}
 	}
 
-	void Insert(int i, int j)
+	void insert(int vertex, int edge)
 	{
-		adjacencyList[i].push_back(j);
-		adjacencyList[j].push_back(i);
+		adjacencyList[vertex].push_back(edge);
+		degree[edge++];	
 	}
-	void search(int start)
+	void sort()
 	{
-		queue.push(start);
-
-		visited[start] = true;
-
-		while (queue.empty() == false)
+		
+	
+		for (int i = 1; i < SIZE; i++)
 		{
+			if (degree[i] == 0) // 진입 차수가 0인 노드를 Queue에 넣는다.
+			{
+				queue.push(i);
+			}
+
 			int print = queue.front();
 
 			queue.pop();
 
 			cout << print << " ";
 
-			for (int i = 0; i < adjacencyList[print].size(); i++)
-			{
-				int next = adjacencyList[print][i];
-				if (visited[next] == false)
-				{
-					queue.push(next);
-
-					visited[next] = true;
-				}
-			}
 		}
-		// 방문한노드를 true로 바꿈
-		// 1에서 시작해서 가장가까운 노드로 접근함2,3 
-		// 임시변수를 만든다
-		// 임시변수에는 방문한 노드의 값을대입하고 출력한다.
+				
 
 	}
 };
 
 int main()
 {
-#pragma region 너비 우선 탐색 (Breadth First Search)
-	// 시작 정점을 방문한 후 시작 정점에 인접한
-	// 모든 정점들을 우선 방문하는 방법입니다.
-
-
-
-	// 더 이상 방문하지 않은 정점이 없을 때까지 방문하지 않은 
-	// 모든 정점들에 대해서도 너비 우선 탐색을 적용합니다.
-
-	Node node;
-
-	node.Insert(1, 2);
-	node.Insert(1, 3);
-
-	node.Insert(2, 4);
-	node.Insert(2, 5);
-
-	node.Insert(3, 6);
-	node.Insert(3, 7);
-
-	node.search(1);
-
+#pragma region 위상 정렬
+	// 병합 그래프에 존재하는 각 정점들의 선행 순서를 지키며,
+	// 모든 정점을 차례대로 진행하는 알고리즘입니다.
+	 
+	// 사이클이 발생하는 경우 위상 정렬을 수행할 수 없습니다.
+	 
+	// DAG(Directed Acyclic Graph) : 사이클이 존재하지 않은 그래프
+	
+	// 시간 복잡도 : O(V + E)
+	
+	// 위상 정렬 하는 방법
+	
+	// 1. 진입 차수가 0인 정점을 Queue에 삽입합니다.
+	 
+	// 2. Queue에서 원소를 꺼내 연결된 모든 간선을 제거합니다.
+	 
+	// 3. 간선 제거 이후에 진입 차수가 0이 된 정점을 Queue에 삽입합니다.
+	 
+	// 4. Queue가 비어있을 때까지 2~ 3번 작업을 반복 수행합니다.
 #pragma endregion
+
+	Graph graph;
+
+	graph.insert(1, 2);
+	graph.insert(1, 5);
+
+	graph.insert(2, 3);
+
+	graph.insert(3, 4);
+
+	graph.insert(4, 6);
+
+	graph.insert(5, 6);
+
+	graph.insert(6, 7);
+
+	graph.sort();
 
 	return 0;
 }
