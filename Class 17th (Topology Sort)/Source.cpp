6b6c9f7ff583c4b780afabeb682eebf66558d933
ParkSeyang <ghlwkd14@naver.com>
2025-04-01 19:44:58 +0900

    (2025.04.01) 위상 정렬

---
+++ Class 17th (Topology Sort)/Source.cpp
@@ -0,0 +1,123 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+#define SIZE 8
+
+class Graph
+{
+private:
+	int degree[SIZE]; // 배열
+	queue<int> queue; // Queue
+	vector<int> adjacencyList[SIZE]; // 인접 리스트
+
+public:
+	Graph()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			degree[i] = 0;
+		}
+	}
+
+	void insert(int vertex, int edge)
+	{
+		adjacencyList[vertex].push_back(edge);
+		degree[edge]++;
+	}
+	void sort()
+	{
+		for (int i = 1; i < SIZE; i++)
+		{
+			if (degree[i] == 0) // 진입 차수가 0인 노드를 Queue에 넣는다.
+			{
+				queue.push(i);
+			}
+
+		}
+
+		//while (!queue.empty())
+		// {
+		// 	int print = queue.front();
+		// 
+		// 	queue.pop();
+		// 
+		// 	cout << print << " ";
+		// 	for (int next: adjacencyList[print])
+		// 	{
+		// 		degree[next]--;
+		// 		if (degree[next] == 0 )
+		// 		{
+		// 			queue.push(next);
+		// 		}
+		// 
+		// 	}
+		// }
+		while (queue.empty() == false)
+		{
+			int print = queue.front();
+			queue.pop();
+
+			cout << print << " ";
+			for (int i = 0; i < adjacencyList[print].size(); i++)
+			{
+				int next = adjacencyList[print][i];
+				degree[next]--;
+				if (degree[next] == 0)
+				{
+					queue.push(next);
+				}
+			}
+		}
+	}
+};
+
+int main()
+{
+#pragma region 위상 정렬
+	// 병합 그래프에 존재하는 각 정점들의 선행 순서를 지키며,
+	// 모든 정점을 차례대로 진행하는 알고리즘입니다.
+
+	// 사이클이 발생하는 경우 위상 정렬을 수행할 수 없습니다.
+
+	// DAG(Directed Acyclic Graph) : 사이클이 존재하지 않은 그래프
+
+	// 시간 복잡도 : O(V + E)
+
+	// 위상 정렬 하는 방법
+
+	// 1. 진입 차수가 0인 정점을 Queue에 삽입합니다.
+
+	// 2. Queue에서 원소를 꺼내 연결된 모든 간선을 제거합니다.
+
+	// 3. 간선 제거 이후에 진입 차수가 0이 된 정점을 Queue에 삽입합니다.
+
+	// 4. Queue가 비어있을 때까지 2~ 3번 작업을 반복 수행합니다.
+#pragma endregion
+
+	Graph graph;
+
+	graph.insert(1, 2);
+	graph.insert(1, 5);
+
+	graph.insert(2, 3);
+
+	graph.insert(3, 4);
+
+	graph.insert(4, 6);
+
+	graph.insert(5, 6);
+
+	graph.insert(6, 7);
+
+	cout << " 위상 정렬 결과 :";
+	graph.sort();
+
+
+
+	return 0;
+}
+
+
