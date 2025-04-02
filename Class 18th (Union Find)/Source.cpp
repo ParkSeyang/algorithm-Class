d8eeed4426a1ee2646372d16f2c6faad470199e7
ParkSeyang <ghlwkd14@naver.com>
2025-04-02 19:45:29 +0900

    (2025.04.02) 계수 정렬

---
+++ Class 18th (Union Find)/Source.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+#define SIZE 6
+
+using namespace std;
+
+int parent[SIZE];
+
+// Root Node 를 찾는 함수
+
+
+int find(int x)
+{
+	// 배열의 인덱스와 값이 같다면 Root Node 발견
+	if (x == parent[x])
+	{
+		return x;
+	}
+	else
+	{
+		// 부모노드의 번호를 전달하면서, Root Node를 찾을 때까지 
+		// 재귀 호출을 반복합니다.
+		return parent[x] = find(parent[x]);
+	}
+}
+
+// 값을 부른후에 부모노드(0번째인덱스)의 값을 자식노드의 값에 갱신합니다.
+void Union(int x, int y)
+{
+	x = find(x);
+	y = find(y);
+
+	if (x == y)
+	{
+		return;
+	}
+
+	if (x < y)
+	{
+		parent[y] = x;
+	}
+	else
+	{
+		parent[x] = y;
+	}
+}
+
+bool same(int x, int y)
+{
+	// 1번
+	if (find(parent[x]) != find(parent[y]))
+	{
+		return false;
+	}
+	else if (find(parent[x]) == find(parent[y]))
+	{
+		return true;
+	}
+	// 2번 좀더 가독성을 높인 코드 
+	// return find(x) == find(y);
+	// 
+	// 3번 테스트안해본코드
+	// if (find(x) != find(y))
+	// {
+	// 	return false;
+	// }
+	// else if (find(x) == find(y))
+	// {
+	// 	return true;
+	// }
+	//find 함수가 같다면 true를 출력하게한다.
+}
+
+int main()
+{
+#pragma region 유니온 파인드
+	// 여러 노드가 존재할 때 어떤 노드가 다른 노드와
+	// 연결되어 있는 지 확인하는 알고리즘 입니다.
+
+	// Union : 특정한 두 개의 노드를 같은 집합으로 합치는 연산입니다.
+
+	// Find : 특정한 노드가 어느 집합에 있는 지 확인하는 연산입니다.
+
+	// int parent[SIZE] = {};
+	for (int i = 0; i < SIZE; i++)
+	{
+		parent[i] = i;
+	}
+	find(1);
+
+	Union(0, 1);
+	Union(3, 4);
+	Union(4, 5);
+
+	same(1, 2);
+
+	cout << same(3, 0) << endl;
+
+	// for (int i = 0; i < SIZE-1; i++)
+	// {
+	// 	cout << same(i, i + 1) << " ";
+	// }
+
+#pragma endregion
+
+	return 0;
+}
+
+
--- Program (Algorithm)/Source.cpp
+++ Program (Algorithm)/Source.cpp
@@ -1,102 +1,76 @@
 ﻿#include <iostream>
-#define SIZE 6
+#include <vector>
+#define SIZE 8
 
 using namespace std;
 
-int parent[SIZE];
-
-// Root Node 를 찾는 함수
-
-
-int find(int x)
+class Kruskal
 {
-	// 배열의 인덱스와 값이 같다면 Root Node 발견
-	if (parent[x] == x)
+private:
+	class Edge
 	{
-		return x;
-	}
-	else
+	private:
+		int vertexX;
+		int vertexY;
+		int weight;
+		
+	public:
+		Edge(int VertexX, int VertexY,int weight)
+		{
+			this->vertexX = VertexX;
+			this->vertexY = VertexY;
+			this->weight = weight;
+			
+		}
+		
+	};
+	int cost;
+	int parent[SIZE];
+	vector<Edge> nodeList;
+public:
+	Kruskal()
 	{
-		// 부모노드의 번호를 전달하면서, Root Node를 찾을 때까지 
-		// 재귀 호출을 반복합니다.
-		return parent[x] = find(parent[x]);
+		cost = 0;
+		for (int i = 0; i < SIZE; i++)
+		{
+			parent[i] = i;
+		}
 	}
-}
 
-// 값을 부른후에 부모노드(0번째인덱스)의 값을 자식노드의 값에 갱신합니다.
-void Union(int x, int y)
-{
-	x = find(x);
-	x = find(y);
-	
-	if (x == y)
+	void insert(int x, int y, int weight)
 	{
-		return;
+		Edge edge(vertexX, vertexY, weight);
+		nodeList.push_back(edge);
+
 	}
 
-	if (x < y)
-	{
-		parent[y] = x;
-	}
-	else
-	{
-		parent[x] = y;
-	}
-}
+};
 
-bool same(int x, int y)
-{
-	// 1번
-	if (find(parent[x]) != find(parent[y]))
-	{
-		return false;
-	}
-	else if (find(parent[x]) == find(parent[y]))
-	{
-		return true;
-	}
-	// 2번
-	// if (find(x) != find(y))
-	// {
-	// 	return false;
-	// }
-	// else if (find(x) == find(y))
-	// {
-	// 	return true;
-	// }
-	//find 함수가 같다면 true를 출력하게한다.
-}
+
 
 int main()
 {
-#pragma region 유니온 파인드
-	// 여러 노드가 존재할 때 어떤 노드가 다른 노드와
-	// 연결되어 있는 지 확인하는 알고리즘 입니다.
+#pragma region 최소 신장 트리
+	// 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는
+	// 부분 그래프로, 그래프의 모든 정점을 최소 비용으로 연결하는 트리입니다.
 
-	// Union : 특정한 두 개의 노드를 같은 집합으로 합치는 연산입니다.
+	// 그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개 입니다.
 
-	// Find : 특정한 노드가 어느 집합에 있는 지 확인하는 연산입니다.
+	Kruskal kruskal
 
-	// int parent[SIZE] = {};
+	kruskal.insert(1, 7, 10);
+	kruskal.insert(4, 7, 14);
+	kruskal.insert(1, 4, 30);
+	kruskal.insert(2, 4, 25);
+	kruskal.insert(1, 2, 64);
+	kruskal.insert(1, 5, 19);
 
-	find(1);
+	kruskal.insert(5, 7, 73);
+	kruskal.insert(2, 5, 61);
+	kruskal.insert(1, 7, );
+	kruskal.insert(1, 7, 10);
+	kruskal.insert(1, 7, 10);
 
-	Union(0, 1);
-	Union(1, 2);
-	Union(3, 4);
-	Union(4, 5);
-
-	same(1, 2);
-
-
-	cout << "0과 1은 연결되어있나? " << same(0, 1) << endl;
-	cout << "0과 4은 연결되어있나? " << same(0, 4) << endl;
-	for (int i = 0; i < SIZE; i++)
-	{
-		parent[i] = i;
-		cout << "  "<< parent[i] << endl;
-	}
-	
 #pragma endregion
 
 	return 0;
