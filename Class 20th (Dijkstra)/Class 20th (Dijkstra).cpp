0324a6d8ff0903126ef576ad4b4ae46ed76ac41b
ParkSeyang <ghlwkd14@naver.com>
2025-04-07 19:33:28 +0900

    (2025.04.07) 퇴각 탐색

---
+++ Class 20th (Dijkstra)/Class 20th (Dijkstra).cpp
@@ -0,0 +1,170 @@
+﻿#include <iostream>
+
+#define SIZE 6
+#define INFINITY 10000000
+
+using namespace std;
+
+class Dijkstra
+{
+private:
+	int node[SIZE][SIZE] =
+	{
+		{0,2,5,1,INFINITY,INFINITY},
+		{2,0,3,2,INFINITY,INFINITY},
+		{5,3,0,3,1,5},
+		{1,2,3,0,1,INFINITY},
+		{INFINITY,INFINITY,1,1,0,2},
+		{INFINITY,INFINITY,5,INFINITY,2,0},
+	};
+	int distance[SIZE];
+	bool visited[SIZE];
+public:
+	Dijkstra()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			visited[i] = false;
+			distance[i] = 0;
+		}
+	}
+
+	const int& Find() // 강사님의 풀이
+	{
+		int index = 0;
+		int min = INFINITY;
+		for (int i = 0; i < SIZE; i++)
+		{
+			if (distance[i] < min && visited[i] == false)
+			{
+				min = distance[i];
+				index = i;
+			}
+		}
+		return index;
+	}
+
+	void calculate(int start)
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			distance[i] = node[start][i];
+		}
+		visited[start] = true;
+
+		for (int i = 0; i < SIZE - 1; i++)
+		{
+			int minNode = Find();
+
+			visited[minNode] = true;
+			for (int j = 0; j < SIZE; j++)
+			{
+				if (visited[j] == false)
+				{
+					if (distance[minNode] + node[minNode][j] < distance[j])
+					{
+						distance[j] = distance[minNode] + node[minNode][j];
+					}
+				}
+			}
+		}
+		for (int i = 0; i < SIZE; i++)
+		{
+			cout << distance[i] << " ";
+		}
+
+	}
+
+	// const int& Find()
+	// {
+	// 	int min = INFINITY;
+	// 	int index = -1;
+	// 	for (int i = 0; i < SIZE; i++)
+	// 	{
+	// 		if (!visited[i] && distance[i] < min)
+	// 		{
+	// 			min = distance[i];
+	// 			index = i;
+	// 		}
+	// 	}
+	// 	
+	// 	return index;
+	// }
+
+
+
+	// void calculate(int start)
+	// {
+	// 	for (int i = 0; i < SIZE; i++)
+	// 	{
+	// 		distance[i] = node[start][i];
+	// 	}
+	// 	visited[start] = true;
+	// 
+	// 	for (int count = 0; count < SIZE - 1; count++)
+	// 	{
+	// 		int next = Find();
+	// 		if (next == -1) // 방문할노드가없을때 
+	// 		{
+	// 			break;
+	// 		}
+	// 		visited[next] = true;
+	// 
+	// 		for (int i = 0; i < SIZE; i++)
+	// 		{
+	// 			if (!visited[i])
+	// 			{
+	// 				if (distance[i]>distance[next] + node[next][i])
+	// 				{
+	// 					distance[i] = distance[next] + node[next][i];
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// 
+	// 	cout << "최단 거리 출력:" << endl;
+	// 	for (int i = 0; i < SIZE; i++)
+	// 	{
+	// 		if (distance[i] == INFINITY)
+	// 			cout << i << ": INF" << endl;
+	// 		else
+	// 			cout << i << ": " << distance[i] << endl;
+	// 	}
+	// 
+	// }
+
+};
+
+
+int main()
+{
+#pragma region 다익스트라 알고리즘
+	// 시적점으로부터 모든 노드까지의 최소 거리를 구해주는
+	// 알고리즘 입니다.
+	// 가중치 그래프를 활용합니다.
+	// 
+	// 1. 거리 배열에 weight[시작 노드]의 값들로 초기화 합니다.
+
+	// 2. 시작점을 방문 처리합니다.
+
+	// 3. 거리 배열에서 최소 비용 노드를 찾고 방문 처리합니다.
+	//    단, 이미 방문한 노드는 제외합니다.
+
+	// 4. 최소 비용 노드를 거쳐갈 지 고민해서 거리 배열을 갱신합니다.
+	//    단, 이미 방문한 노드는 제외합니다.
+
+	// 5. 모든 노드를 방문할 때까지 3번 ~ 4번을 반복합니다.
+
+	// 방문하지 않은 노드 중에서 가장 작은 거리를 가진 노드를
+	// 방문하고, 그 노드와 연결된 다른 노드까지의 거리를 계산합니다.
+
+	Dijkstra dijkstra;
+	dijkstra.calculate(0);
+
+
+#pragma endregion
+
+	return 0;
+}
+
+
--- Program (Algorithm)/Source.cpp
+++ Program (Algorithm)/Source.cpp
@@ -1,117 +1,21 @@
 ﻿#include <iostream>
 
-#define SIZE 6
-#define INFINITY 10000000
-
 using namespace std;
-
-class Dijkstra
-{
-private:
-	int node[SIZE][SIZE] =
-	{
-		{0,2,5,1,INFINITY,INFINITY},
-		{2,0,3,2,INFINITY,INFINITY},
-		{5,3,0,3,1,5},
-		{1,2,3,0,1,INFINITY},
-		{INFINITY,INFINITY,1,1,0,2},
-		{INFINITY,INFINITY,5,INFINITY,2,0},
-	};
-	int distance[SIZE];
-	bool visited[SIZE];
-public:
-	Dijkstra()
-	{
-		for (int i = 0; i < SIZE; i++)
-		{
-			visited[i] = false;
-			distance[i] = 0;
-		}
-	}
-	const int& Find()
-	{
-		int min = INFINITY;
-		int index = -1;
-		for (int i = 0; i < SIZE; i++)
-		{
-			if (!visited[i] && distance[i] < min)
-			{
-				min = distance[i];
-				index = i;
-			}
-		}
-		
-		return index;
-	}
-	void calculate(int start)
-	{
-		for (int i = 0; i < SIZE; i++)
-		{
-			distance[i] = node[start][i];
-		}
-		visited[start] = true;
-
-		for (int count = 0; count < SIZE - 1; count++)
-		{
-			int next = Find();
-			if (next == -1)
-			{
-				break;
-			}
-			visited[next] = true;
-
-			for (int i = 0; i < SIZE; i++)
-			{
-				if (!visited[i])
-				{
-					if (distance[i]>distance[next] + node[next][i])
-					{
-						distance[i] = distance[next] + node[next][i];
-					}
-				}
-			}
-		}
-
-		cout << "최단 거리 출력:" << endl;
-		for (int i = 0; i < SIZE; i++)
-		{
-			if (distance[i] == INFINITY)
-				cout << i << ": INF" << endl;
-			else
-				cout << i << ": " << distance[i] << endl;
-		}
-
-	}
-
-};
 
 
 int main()
 {
-#pragma region 다익스트라 알고리즘
-	// 시적점으로부터 모든 노드까지의 최소 거리를 구해주는
-	// 알고리즘 입니다.
-	// 가중치 그래프를 활용합니다.
-	// 
-	// 1. 거리 배열에 weight[시작 노드]의 값들로 초기화 합니다.
+#pragma region 퇴각 검색
+	// 해를 찾아가는 도중에 지금 경로가 해가 될 것 같지 않으면,
+	// 더 이상 깊이 들어가지 않고, 이전 단계로 다시 들어가는 알고리즘입니다.
 
-	// 2. 시작점을 방문 처리합니다.
+	// 퇴각 검색의 유망성 판단
+	// 해가 될 만한지 판단한 후에 유망하지 않다고 결정되면,
+	// 그 노드의 이전 노드로 돌아가 다음 자식 노드로 이동합니다.
 
-	// 3. 거리 배열에서 최소 비용 노드를 찾고 방문 처리합니다.
-	//    단, 이미 방문한 노드는 제외합니다.
+	// 해가 될 만한 기능이 있으면 유망하다. (ProMising)
+	// 유망하지 않은 노드에 가지 않는 것 (Pruning)
 
-	// 4. 최소 비용 노드를 거쳐갈 지 고민해서 거리 배열을 갱신합니다.
-	//    단, 이미 방문한 노드는 제외합니다.
-
-	// 5. 모든 노드를 방문할 때까지 3번 ~ 4번을 반복합니다.
-
-	// 방문하지 않은 노드 중에서 가장 작은 거리를 가진 노드를
-	// 방문하고, 그 노드와 연결된 다른 노드까지의 거리를 계산합니다.
-	
-	Dijkstra dijkstra;
-	dijkstra.calculate(0);
-	
-	
 #pragma endregion
 
 	return 0;
